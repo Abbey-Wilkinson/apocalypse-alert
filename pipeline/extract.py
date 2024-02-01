08799592f800267cb0f07a09a602446b43ed557f
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2024-02-01 15:25:44 +0000

    Included earthquake API, multithreading and session manager

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,30 +1,51 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from os import environ
+from time import perf_counter
 
 from dotenv import load_dotenv
 import pandas as pd
-import requests
+import polars as pl
+from requests import Session
 
+session = Session()
+
+
+# World Cities
+
+def get_capital_cities():
+    """
+    Loads the capital city of each country.
+    """
+
+    with open("worldcities.csv", "r") as file:
+        cities = pd.read_csv(file)
+
+        cities = cities[cities["capital"] == "primary"]
+
+        city_names = cities["city"].values.tolist()
+
+    return city_names
+
+
+def get_capital_cities_pl():
+    """
+    Loads the capital city of each country.
+    """
+    cities = pl.read_csv("worldcities.csv", columns=[
+                         'city', 'country', 'capital'])
+    capital_cities = cities.filter(pl.col("capital") == "primary")['city']
+    return capital_cities
+
+
+# Air Pollution API
 
 def get_pollution_api_request_response(city: str, key: str) -> dict:
     """
     Retrieve the response json data for an API request to the pollution api.
     """
 
-    response = requests.get(f'https://api.waqi.info/feed/{city}/?token={key}')
+    response = session.get(f'https://api.waqi.info/feed/{city}/?token={key}')
     json_data = response.json()
-
-    return json_data
-
-
-def get_uv_api_request_response(key: str, lat: str, lng: str) -> dict:
-    """
-    Retrieve the response json data for an API request to the uv api.
-    """
-
-    response = requests.get(
-        f'https://api.openuv.io/api/v1/uv?lat={lat}&lng={lng}', headers={'x-access-token': key})
-    json_data = response.json()
-
     return json_data
 
 
@@ -41,97 +62,93 @@ def get_pollution_info(pollution_data: dict) -> dict:
             "general_aqi": pollution_data['data']['aqi'],
             "city": pollution_data['data']['city']['name'],
             "lat": pollution_data['data']['city']['geo'][0],
-            "lng": pollution_data['data']['city']['geo'][1]
+            "lng": pollution_data['data']['city']['geo'][1],
+            "individual_pollutant_data": pollution_data['data']['iaqi']
         }
 
         return wanted_pollution
     return "No data found."
 
 
-def get_individual_pollutant_info(pollution_data: dict) -> dict:
-    """
-    Gets the required data for each individual pollutant.
-    """
-
-    if pollution_data["status"] == "ok":
-        individual_pollutant_data = pollution_data['data']['iaqi']
-        return individual_pollutant_data
-    return "No data found."
-
-
-def get_capital_cities():
-    """
-    Loads the capital city of each country.
-    """
-
-    with open("worldcities.csv", "r") as file:
-       cities = pd.read_csv(file)
-
-       cities = cities[cities["capital"] == "primary"]
-
-       city_names = cities["city"].values.tolist()
-    
-    return city_names
-
-
-def get_air_reading_data_for_all(capital_cities: str):
+def get_air_reading_data_for_all(capital_cities: pl.Series):
     """
     Retrieves all of the information on a capital city,
     if the endpoint is valid then the desired information is extracted.
     """
-    
-    wanted_city_data = []
-    for city in capital_cities:
-        print(f"Getting data for {city}...")
-        all_city_data = get_pollution_api_request_response(city, environ["POLLUTION_API_KEY"])
-        city_data = get_pollution_info(all_city_data)
+    with ThreadPoolExecutor() as executor:
 
-        if city_data != "No data found.":
-            wanted_city_data.append(city_data)
+        futures = [executor.submit(get_pollution_api_request_response,
+                                   city, environ["POLLUTION_API_KEY"]) for city in capital_cities]
+
+        wanted_city_data = []
+        for future in as_completed(futures):
+            all_city_data = future.result()
+            city_data = get_pollution_info(all_city_data)
+
+            if city_data != "No data found.":
+                wanted_city_data.append(city_data)
 
     return wanted_city_data
 
-        
-def get_uv_index_data_for_all(cities: list[dict]) -> list[dict]:
+
+# Earthquake API
+
+def get_earthquake_api_request_response(lat: str, lng: str) -> dict:
+    """
+    Retrieve the response json data for an API request to the uv api.
+    """
+
+    response = session.get(
+        f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude={lat}&longitude={lng}&maxradiuskm=1000')
+    json_data = response.json()
+
+    features = json_data.get("features", [])
+
+    if features:
+        feature = features[0]
+        properties = feature.get('properties', {})
+
+        return {
+            "magnitude": properties['mag'],
+            "country": properties['place'],
+            "at": properties['time']
+        }
+
+    return None
+
+
+def get_earthquake_data_for_all(cities: list[dict]) -> list[dict]:
     """
     Retrieves uv data for all capital cities.
     """
 
-    all_uv_data = []
-    for city in cities:
-        print(f"Getting data for {city['city']}...")
-        uv_data = get_uv_api_request_response(environ['UV_API_KEY'], city['lat'], city['lng'])
+    with ThreadPoolExecutor() as executor:
 
-        all_uv_data.append(uv_data)
-    
-    return all_uv_data
+        futures = [executor.submit(get_earthquake_api_request_response,
+                                   city['lat'], city['lng']) for city in cities]
+
+        all_earthquake_data = []
+        for future in as_completed(futures):
+            earthquake_detail = future.result()
+
+            if earthquake_detail:
+                all_earthquake_data.append(earthquake_detail)
+
+        return all_earthquake_data
 
 
 if __name__ == "__main__":
 
     load_dotenv()
+    start = perf_counter()
 
-
-    capital_cities = get_capital_cities()
-    # print(capital_cities)
-
-    # pollution_data = get_pollution_api_request_response( capital_cities[1], environ["POLLUTION_API_KEY"])
-
-    # relevant_pollution_data = get_pollution_info(pollution_data)
-    # individual_pollutant_data = get_individual_pollutant_info(
-    #     pollution_data)
-
-    # print(relevant_pollution_data)
-    # print(individual_pollutant_data)
-
-    # uv_data = get_uv_api_request_response(environ['UV_API_KEY'], relevant_pollution_data['lat'], relevant_pollution_data['lng'])
-
-    # print(uv_data)
-
-
+    capital_cities = get_capital_cities_pl()
     wanted_city_data = get_air_reading_data_for_all(capital_cities)
+    all_earthquake_index_data = get_earthquake_data_for_all(
+        wanted_city_data)
+
     print(wanted_city_data)
+    print(all_earthquake_index_data)
 
-
-    all_uv_index_data = get_uv_index_data_for_all(wanted_city_data)
-    print(all_uv_index_data)
+    end = perf_counter() - start
+    print(f"Total time taken: {end}s")
