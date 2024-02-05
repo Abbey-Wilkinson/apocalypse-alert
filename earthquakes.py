5b3670e8e2e880008be540ab5417f80be9f20e15
Anu Kaur <anukaur@Anus-MacBook-Pro.local>
2024-02-05 22:38:37 +0000

    just some basic additions

--- earthquakes.py
+++ earthquakes.py
@@ -1,7 +1,15 @@
-
+import requests
 import pandas as pd
 import polars as pl
 
-world_cities = pd.read_csv("worldcities.csv")
+countries_csv = pd.read_csv("worldcities.csv")
 
-print(world_cities)
+country_searched = "India"
+lng = countries_csv.loc[countries_csv["country"] == country_searched, 'lng'].values[0]
+lat = countries_csv.loc[countries_csv["country"] == country_searched, 'lat'].values[0]
+
+response = requests.get(f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude={lat}&minlongitude={lng}")
+json_data = response.json()
+# print(json_data["features"][0]["properties"].keys())
+print(lng)
+print(lat)
