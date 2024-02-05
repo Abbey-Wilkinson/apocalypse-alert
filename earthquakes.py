abed30a48f9add0fec49a4acf84a44cacdb208c2
Anu Kaur <anukaur@Anus-MacBook-Pro.local>
2024-02-05 16:27:45 +0000

    EQ API initial

--- earthquakes.py
+++ earthquakes.py
@@ -1,21 +1,7 @@
-'''Extract + make use of information from earthquakes API.'''
-from geopythonukparliament import Countries
 
-def get_lat_lon_from_country(country_name):
-    countries = Countries()
-    country_data = countries.get_country_by_name(country_name)
-    
-    if country_data:
-        latitude, longitude = country_data['latitude'], country_data['longitude']
-        return latitude, longitude
-    else:
-        print(f"Location information not found for {country_name}")
-        return None
+import pandas as pd
+import polars as pl
 
-# Example usage
-country_name = 'United States'
-result = get_lat_lon_from_country(country_name)
+world_cities = pd.read_csv("worldcities.csv")
 
-if result:
-    latitude, longitude = result
-    print(f"Latitude: {latitude}, Longitude: {longitude}")
+print(world_cities)
